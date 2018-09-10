0e4488a7f06391b0bbd53ecdc21f8678c5b439f3
Kirill <immotional@aol.com>
2018-09-10 19:26:17 +0100

    docker hot-reload fix

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/health", (req, res) => res.json({status: "app running"}));
-
 app.get("/values", (req, res) =>
   valueService.getAll().then(values => res.json({values}))
 );
@@ -20,7 +18,7 @@ app.post("/values", ({body}, res) =>
   valueService
     .calculate(parseInt(body.index))
     .then(() => res.status(201).json({status: "created"}))
-    .catch(({ status = 500, message }) => res.status(status).send(message))
+    .catch(({ status = 500, message }) => res.status(status).json({message}))
 );
 
 app.listen(keys.serverPort, () => console.log(`listening on ${keys.serverPort}`));
